0997cf29d23a1b469ca8d8076f32d47057cd3225
Armaghan Behlum <armaghan23@gmail.com>
2017-05-06 11:29:21 -0400

    Part 1 complete

---
+++ jokerlib/src/main/java/com/example/Joker.java
@@ -0,0 +1,16 @@
+package com.example;
+
+/**
+ * Joke generating class
+ */
+public class Joker {
+
+    /**
+     * Gets a joke
+     *
+     * @return a string that is the joke
+     */
+    public String getJoke() {
+        return "Hey I'm Mary Poppins y'all";
+    }
+}
